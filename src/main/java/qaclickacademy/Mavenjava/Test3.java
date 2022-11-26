c93eeda8683e0418afd723b0ea4e23b0f0ab76e9
Nevena <nk130005@gmail.com>
2022-11-26 22:34:07 +0100

    dva

--- src/main/java/qaclickacademy/Mavenjava/Test3.java
+++ src/main/java/qaclickacademy/Mavenjava/Test3.java
@@ -1,12 +1,13 @@
 package qaclickacademy.Mavenjava;
 
+
 import org.junit.Test;
 
 public class Test3 {
 @Test
 	public void Third() {
 		System.out.println("Star");
-	System.out.println("Starica");
+	System.out.println("Staric");
 	System.out.println("Starac");
 
 	}
